7231d5b9c098bad7fe126dd64118879776a85f70
EJ <etovarda@my.centennialcollege.ca>
2023-03-31 16:34:31 -0400

    TailwindCSS forms plugin
    **Se mantiene este plugin ya que remueve estilos por defecto, reemplazandolo por estilos basicos necesarios para que se vean bien los formularios

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'base' //only generate global styles
+    })],
+};
 
